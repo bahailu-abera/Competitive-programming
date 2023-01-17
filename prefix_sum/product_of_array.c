2f565de05d0aa4bb191914a47a3639122ee8e3be
bahailu <aberabahailu4@gmail.com>
2023-01-18 02:23:45 +0300

    product of array except it self

---
+++ prefix_sum/product_of_array.c
@@ -0,0 +1,17 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* productExceptSelf(int* nums, int numsSize, int* returnSize){
+    int *answer;
+    int mul = 1, index;
+
+    answer = malloc(numsSize * sizeof(int));
+    for (index = 0; index < numsSize; index++)
+        answer[index] = mul, mul *= nums[index];
+    mul = nums[--index];
+    for (--index; index >= 0; index--)
+        answer[index] *= mul, mul *= nums[index];
+    *returnSize = numsSize;
+
+    return (answer);
+}
