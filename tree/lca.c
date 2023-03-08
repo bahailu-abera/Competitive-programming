0b909bb86704948b1247d84d8399ef00d804c1b9
bahailu <aberabahailu4@gmail.com>
2023-03-08 11:09:25 +0300

    lowest common ancestor in a bst

---
+++ tree/lca.c
@@ -0,0 +1,21 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+struct TreeNode* lowestCommonAncestor(struct TreeNode* root, struct TreeNode* p, struct TreeNode* q) {
+    while (root)
+    {
+        if (p->val < root->val && q->val < root->val)
+            root = root->left;
+        else if (p->val > root->val && q->val > root->val)
+            root = root->right;
+        else
+            return (root);
+    }
+    return (NULL);
+}
