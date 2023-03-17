e5fdf43206ac32efc8c4cfc97f6a762ef32c58c4
bahailu <aberabahailu4@gmail.com>
2023-03-17 10:54:40 +0300

    updated

--- tree/flip_equivalent_bt.c
+++ tree/flip_equivalent_bt.c
@@ -12,15 +12,9 @@ bool flipEquiv(struct TreeNode* root1, struct TreeNode* root2){
         if (root1->val != root2->val)
             return (false);
 
-        if (!flipEquiv(root1->left, root2->right))
-        {
-            if (!flipEquiv(root1->left, root2->left))
-                return (false);
-
-            return flipEquiv(root1->right, root2->right);
-        }
-
-        return flipEquiv(root1->right, root2->left);
+        return (flipEquiv(root1->left, root2->left) && flipEquiv(root1->right, root2->right)) ||
+		(flipEquiv(root1->left, root2->right) && flipEquiv(root1->right, root2->left));
     }
+
     return (root1 == root2);
 }
