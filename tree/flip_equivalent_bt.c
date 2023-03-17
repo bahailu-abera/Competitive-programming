6b104fc14cb812df262cf988842561649fb5937d
bahailu <aberabahailu4@gmail.com>
2023-03-17 10:36:07 +0300

    flip equivalent binary tree

---
+++ tree/flip_equivalent_bt.c
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+bool flipEquiv(struct TreeNode* root1, struct TreeNode* root2){
+    if (root1 && root2)
+    {
+        if (root1->val != root2->val)
+            return (false);
+
+        if (!flipEquiv(root1->left, root2->right))
+        {
+            if (!flipEquiv(root1->left, root2->left))
+                return (false);
+
+            return flipEquiv(root1->right, root2->right);
+        }
+
+        return flipEquiv(root1->right, root2->left);
+    }
+    return (root1 == root2);
+}
