8cb1e43e90e3eb0043d1deafdd6dc378fff05425
bahailu <aberabahailu4@gmail.com>
2023-03-23 20:46:10 +0300

    kth largest number

--- sorting/kth_largest_number.py
+++ sorting/kth_largest_number.py
@@ -1,7 +1,41 @@
-class Compare(str):
-    def __lt__(x, y):
-        return int(x) < int(y)
 class Solution:
-    def kthLargestNumber(self, nums: List[str], k: int) -> str:
-        nums = sorted(nums, key=Compare)
-        return nums[len(nums) - k]
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        k = len(nums) - k
+        def partition(low, high):
+
+            # choose the rightmost element as pivot
+            pivot = nums[high]
+
+            # pointer for greater element
+            i = low - 1
+
+            # traverse through all elements
+            # compare each element with pivot
+            for j in range(low, high):
+                if nums[j] <= pivot:
+                    # if element smaller than pivot is found
+                    # swap it with the greater element pointed by i
+                    i = i + 1
+
+                    # swapping element at i with element at j
+                    (nums[i], nums[j]) = (nums[j], nums[i])
+
+            # swap the pivot element with the greater element specified by i
+            (nums[i + 1], nums[high]) = (nums[high], nums[i + 1])
+
+            # return the position from where partition is done
+            return i + 1
+
+        right = len(nums) - 1
+        left = 0
+        pivot = partition(left, right)
+
+        while pivot != k:
+            if pivot < k:
+                left = pivot + 1
+            else:
+                right = pivot - 1
+
+            pivot = partition(left, right)
+
+        return nums[pivot]
