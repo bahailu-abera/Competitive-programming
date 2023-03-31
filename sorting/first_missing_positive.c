d9ed9bdc92e25614306ce2cafb68cfa34336fb91
Bahailu Abera <aberabahailu4@gmail.com>
2023-03-31 22:19:11 +0300

    First Missing Positive

---
+++ sorting/first_missing_positive.c
@@ -0,0 +1,22 @@
+int firstMissingPositive(int* nums, int numsSize){
+    int i, tmp;
+
+    for (i = 0; i < numsSize; )
+    {
+        if (nums[i] > 0 && nums[i] <= numsSize && nums[nums[i] - 1] != nums[i])
+        {
+            tmp = nums[nums[i] - 1], nums[nums[i] - 1] = nums[i], nums[i] = tmp;
+        }
+
+        else
+            i++;
+    }
+
+    for (i = 0; i < numsSize; i++)
+    {
+        if (nums[i] != i + 1)
+            return (i + 1);
+    }
+
+    return (numsSize + 1);
+}
