6b52f91c310e959ed06a539bc7b95cd2cc53de7d
Bahailu Abera <aberabahailu4@gmail.com>
2023-04-05 00:21:56 +0300

    Updated: use dummy node

--- linked_list/merge_two_sorted_linked_list.c
+++ linked_list/merge_two_sorted_linked_list.c
@@ -5,58 +5,18 @@
  *     struct ListNode *next;
  * };
  */
-
-
 struct ListNode* mergeTwoLists(struct ListNode* list1, struct ListNode* list2){
-    struct ListNode *result, *new, *head;
+    struct ListNode dummy = {0}, *tmp = &dummy;
 
-    if (list1 == NULL)
-        return (list2);
-    if (list2 == NULL)
-        return (list1);
-    result = NULL;
-    while (list1 != NULL && list2 != NULL)
+    while (list1 && list2)
     {
-        new = malloc(sizeof(struct ListNode));
-        if (new == NULL)
-            return (NULL);
-        if (list1->val <= list2->val)
-        {
-            new->val = list1->val;
-            new->next = NULL;
-            list1 = list1->next;
-        }
+        if (list1->val < list2->val)
+            tmp->next = list1, list1 = list1->next;
         else
-        {
-            new->val = list2->val;
-            new->next = NULL;
-            list2 = list2->next;
-        }
-        if (result == NULL)
-            result = new, head = result;
-        else
-            result->next = new, result = result->next;
+            tmp->next = list2, list2 = list2->next;
+        tmp = tmp->next;
     }
-    while (list1 != NULL)
-    {
-        new = malloc(sizeof(struct ListNode));
-        if (new == NULL)
-            return (NULL);
-        new->val = list1->val;
-        new->next = NULL;
-        list1 = list1->next;
-        result->next = new, result = result->next;
-    }
-    while (list2 != NULL)
-    {
-        new = malloc(sizeof(struct ListNode));
-        if (new == NULL)
-            return (NULL);
-        new->val = list2->val;
-        new->next = NULL;
-        list2 = list2->next;
-        result->next = new, result = result->next;
-    }
-    return (head);
-    
+    tmp->next = (list1 ? list1: list2);
+
+    return (dummy.next);
 }
