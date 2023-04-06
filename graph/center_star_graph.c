38ceb180393ba7ce7c134ed9700895a8b5461c93
Bahailu Abera <aberabahailu4@gmail.com>
2023-04-06 18:09:05 +0300

    Find Center of Star Graph

---
+++ graph/center_star_graph.c
@@ -0,0 +1,7 @@
+int findCenter(int** edges, int edgesSize, int* edgesColSize){
+
+    if (edges[0][0] == edges[1][0] || edges[0][0] == edges[1][1])
+        return (edges[0][0]);
+
+    return (edges[0][1]);
+}
