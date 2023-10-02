79d5e0139852b2edd849de7ff24f941b1bc1cb6e
Bahailu Abera <aberabahailu4@gmail.com>
2023-10-03 00:49:45 +0300

    lowest-common-ancestor-of-a-binary-search-tree

---
+++ tree/lowest_common_ancestor.c
@@ -0,0 +1,17 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+struct TreeNode* lowestCommonAncestor(struct TreeNode* root, struct TreeNode* p, struct TreeNode* q) {
+    if (p->val < root->val && q->val < root->val)
+        return lowestCommonAncestor(root->left, p, q);
+    else if (p->val > root->val && q->val > root->val)
+        return lowestCommonAncestor(root->right, p, q);
+
+    return (root);
+}
